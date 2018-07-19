363a671ac971f0bd0f4ffbada6a86c4ffd97bc8e
Maros Zeleny <maros.zeleny@globallogic.com>
2018-07-19 15:58:19 +0200

    add component MenuSideBar

--- src/components/MenuSideBar/index.js
+++ src/components/MenuSideBar/index.js
@@ -1,21 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./menu.scss";
+import styles from "./menusidebar.scss";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import withStyles from "@material-ui/core/styles/withStyles";
+import classnames from "classnames";
 
-export default function MenuSideBar() {
-  return (
-    <div className={styles.menu}>
-      <ul>
-        <li>
-          <Link to="/customers"> Customer </Link>
-        </li>
-        <li>
-          <Link to="/products"> Products </Link>
-        </li>
-        <li>
-          <Link to="/orders">Orders </Link>
-        </li>
-      </ul>
-    </div>
-  );
-}
+const activeStyles = {
+  MenuItem: {
+    color: "#17a2b8",
+    "&:focus": {
+      color: "#000",
+      backgroundColor: "#007bff",
+      textDecoration: "underline"
+    }
+  },
+  MuiList: {
+    padding: 0
+  }
+};
+const MenuSideBar = ({ classes }) => (
+  <MenuList className={classnames(styles.menuSidebar, classes.MuiList)}>
+    <Link to="/customers">
+      <MenuItem className={classes.MenuItem}>Customers</MenuItem>
+    </Link>
+    <Link to="/products">
+      <MenuItem className={classes.MenuItem}>Products</MenuItem>
+    </Link>
+    <Link to="/orders">
+      <MenuItem className={classes.MenuItem}>Orders</MenuItem>
+    </Link>
+  </MenuList>
+);
+
+export default withStyles(activeStyles)(MenuSideBar);
