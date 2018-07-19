e1ef3b711c99850f2ac7d6bd3148ed4cd20694a0
Maros Zeleny <maros.zeleny@globallogic.com>
2018-07-19 15:44:50 +0200

    addin sass styles and font roboto

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#cce5ff",
+    },
+    secondary: {
+      main: "#f8d7da"
+    }
+  }
+});
+
+export default theme;
