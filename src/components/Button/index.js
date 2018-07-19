d4ad8891c01a6b7bd3f7863c5654590a852f77ba
Maros Zeleny <maros.zeleny@globallogic.com>
2018-07-19 16:02:09 +0200

    add component Button and UploadButton

---
+++ src/components/Button/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button as MaterialButton } from "@material-ui/core";
+
+const buttonPallete = {
+  success: "#d4edda",
+  info: "#d1ecf1",
+  primary: "#cce5ff",
+  danger: "#FF0000",
+  lightGrey: "#e2e3e5",
+  materialBlue: "#2196f3"
+};
+
+const Button = ({
+  children,
+  color,
+  variant,
+  onClick,
+  ...props
+}) => {
+  const buttonStyles = {
+    background: buttonPallete[color],
+  };
+  return (
+    <MaterialButton
+      color={color}
+      style={buttonStyles}
+      variant={variant}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </MaterialButton>
+  );
+};
+Button.propTypes = {
+  color: PropTypes.string,
+  variant: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired
+};
+Button.defaultProps = {
+  onClick: () => {},
+  color: "primary",
+  variant: "contained"
+};
+
+export default Button;
---
+++ src/components/Button/uploadButton.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Image from "components/Image";
+import PropTypes from "prop-types";
+import Button from "components/Button";
+import Alert from "components/Alert";
+import styles from "./uploadButton.scss";
+import { AddAPhoto } from "../../assets/material-ui-icons";
+
+const UploadButton = ({
+  classes,
+  handleInputChange,
+  photo,
+  validate,
+  photoName,
+  validationPhotoError
+}) => {
+  return (
+    <div className={styles.uploadButton}>
+      <input
+        accept="image/*"
+        className={classes.uploadInput}
+        id="photo"
+        type="file"
+        onChange={event => {
+          handleInputChange(event);
+          validate.isRequired({ [event.target.id]: event.target.files[0] });
+        }}
+      />
+      <label htmlFor="photo">
+        <Button color="materialBlue" variant="contained" component="span">
+          <AddAPhoto className={classes.iconLeft} />
+          Upload Photo
+        </Button>
+      </label>
+      <div className={styles.uploadButton__text}>
+        {validationPhotoError.length !== 0 && (
+          <Alert>{validationPhotoError}</Alert>
+        )}
+      </div>
+      <div className={styles.uploadButton__image}>
+        {photo !== null && (
+          <Image
+            src={typeof photo === "object" ? URL.createObjectURL(photo) : photo}
+            width={127}
+            height={127}
+            alt={photoName}
+            fileName={photoName}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+UploadButton.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  photo: PropTypes.object.isRequired,
+  photoName: PropTypes.string,
+  classes: PropTypes.object,
+  validate: PropTypes.objectOf(PropTypes.func),
+  validationPhotoError: PropTypes.string
+};
+UploadButton.defaultProps = {
+  photoName: "",
+  validate: {},
+  validationPhotoError: ""
+};
+
+export default UploadButton;
