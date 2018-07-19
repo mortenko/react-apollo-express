a60500d2c37ec97ebb8387aed21a29766a64d617
Maros Zeleny <maros.zeleny@globallogic.com>
2018-07-19 16:12:39 +0200

    add component Table

---
+++ src/components/Table/index.js
@@ -0,0 +1,5 @@
+export { default as Table } from "@material-ui/core/Table";
+export { default as TableHead } from "@material-ui/core/TableHead";
+export { default as TableBody } from "@material-ui/core/TableBody";
+export { default as TableCell } from "@material-ui/core/TableCell";
+export {default as TableRow } from "@material-ui/core/TableRow";
